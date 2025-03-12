4eb0e133532fe716565e4e1a441b819dede7cae4
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-03-12 10:50:45 +0100

    Clean up

--- app/db/model.py
+++ app/db/model.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 from decimal import Decimal
-from typing import Any, ClassVar, Optional
+from typing import Any, ClassVar
 from uuid import UUID
 
 from sqlalchemy import (
