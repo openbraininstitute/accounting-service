b161ff4c3e49860f00c29bca41d4c659fd721a73
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-03-24 08:20:10 +0100

    Create a top up transaction when creating account with non-zero initial balance

--- app/service/account.py
+++ app/service/account.py
@@ -3,8 +3,10 @@
 from decimal import Decimal
 from uuid import UUID
 
+from app.constants import D0
 from app.repository.group import RepositoryGroup
 from app.schema.domain import ProjAccount, SysAccount, VlabAccount
+from app.service.budget import top_up
 
 
 async def add_system(repos: RepositoryGroup, account_id: UUID, name: str) -> SysAccount:
@@ -16,7 +18,13 @@ async def add_virtual_lab(
     repos: RepositoryGroup, account_id: UUID, name: str, balance: Decimal
 ) -> VlabAccount:
     """Add a new virtual lab account."""
-    return await repos.account.add_vlab_account(account_id=account_id, name=name, balance=balance)
+    account = await repos.account.add_vlab_account(account_id=account_id, name=name)
+
+    if balance == D0:
+        return account
+
+    await top_up(repos, vlab_id=account.id, amount=balance)
+    return await repos.account.get_vlab_account(vlab_id=account.id)
 
 
 async def add_project(
