88980b3ca4a3afbe721b605a46753b189fa8940a
Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>
2026-07-03 15:36:27 +0200

    Increase DB_MAX_OVERFLOW (#50)

--- app/config.py
+++ app/config.py
@@ -86,7 +86,7 @@ class Settings(BaseSettings):
 
     DB_POOL_SIZE: int = 30
     DB_POOL_PRE_PING: bool = True
-    DB_MAX_OVERFLOW: int = 0
+    DB_MAX_OVERFLOW: int = 20
 
     @field_validator("DB_URI", mode="before")
     @classmethod
